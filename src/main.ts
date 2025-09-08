4bd596d3e6768be721fc4475c4125982eed4de30
yoonsooho <7552678@naver.com>
2025-09-09 02:47:14 +0900

    feat : cors 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,13 @@ async function bootstrap() {
 
   // CORS 설정 추가
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://goaldiary.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
+
+  const port = process.env.PORT || 3001;
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -21,6 +23,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3001);
+  await app.listen(port, '0.0.0.0'); // 0.0.0.0 꼭 필요
 }
 bootstrap();
