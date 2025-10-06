368e475928c2cb992ca38a91e9a5eae662b36ca3
yoonsooho <7552678@naver.com>
2025-10-06 15:04:32 +0900

    fix : render server ping api 생성

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('ping')
+  getPing(): { status: string } {
+    return this.appService.getPing();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,7 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  getPing(): { status: string } {
+    return { status: 'alive' };
+  }
 }
